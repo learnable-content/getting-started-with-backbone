cd6f44039139da4b4c737e2ca00debec2ddde815
bodrovis <ibodrov@campaigner.ru>
2015-06-15 14:36:31 +0300

    final

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 Organizer = {
   initialize: function() {
-    var eventsList = new Organizer.EventsListView();
-    eventsList.render();
+    var events = new Organizer.Events();
+    var eventsList = new Organizer.EventsListView({
+      collection: events
+    });
+    events.fetch({
+      success: function() {
+        events.reset([ {title: 'test1'}, {title: 'test2'}, {title: 'test3'} ]);
+        eventsList.render();
+      },
+      error: function() {
+        console.error('error');
+      }
+    });
   }
 };
 

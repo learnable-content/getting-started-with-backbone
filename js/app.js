e24fbc86ee0eb3bd047b6ac7904fbd8efb7c8c32
bodrovis <ibodrov@campaigner.ru>
2015-06-15 17:31:23 +0300

    final

--- js/app.js
+++ js/app.js
@@ -1,14 +1,5 @@
 Organizer = {
   initialize: function() {
-    Organizer.events = new Organizer.Events();
-
-    new Organizer.NewEventView();
-
-    new Organizer.EventsListView({
-      collection: Organizer.events
-    });
-    Organizer.events.fetch();
-
     Organizer.router = new Organizer.Router();
     Backbone.history.start();
   }

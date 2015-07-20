f0b437d88ddb2cd4771d73d88e1c37bde81706ec
Angela Molina <angela.molina@learnable.com>
2015-07-20 13:17:57 +1000

    Added lesson resources

---
+++ js/views/events.js
@@ -0,0 +1,25 @@
+Organizer.EventsListView = Backbone.View.extend({
+  render: function() {
+    var events = [
+      {title: 'go to work'},
+      {title: 'meet with friends'},
+      {title: 'visit grandma'}
+    ];
+    var events_elements = [];
+    _.each(events, function(event) {
+      var eventView = new Organizer.EventView({model: event});
+      events_elements.push(eventView.render().el);
+    });
+    this.$el.append(events_elements);
+    $('#app').html(this.el);
+  },
+  tagName: 'ul'
+});
+
+Organizer.EventView = Backbone.View.extend({
+  tagName: 'li',
+  render: function() {
+    this.$el.html(this.model.title);
+    return this;
+  }
+});
